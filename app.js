92f11def312b2e699c2ad65f5bb43b0983a13869
Kenji Hirabayashi <mkhira2@gmail.com>
2017-06-28 11:38:28 -0500

    moved findAll function into a .then method

--- app.js
+++ app.js
@@ -3,21 +3,20 @@ const models = require("./models/");
 
 
 const user = models.User.build({
-  name: // enter name,
-  email: // enter email,
-  bio: // enter bio
+  name: 'Susan',
+  email: 'susan@susan.com',
+  bio: 'im a susan and you are not'
 });
 
-user.save()
+user.save().then(function() {
+  models.User.findAll()
+    .then( function(users) {
 
+      for (var i = 0; i < users.length; i++) {
+        console.log("We found " + users[i].name )
+      }
 
-models.User.findAll()
-  .then( function(users) {
+      models.sequelize.close()
 
-    for (var i = 0; i < users.length; i++) {
-      console.log("We found " + users[i].name )
-    }
-
-    models.sequelize.close()
-
-  })
+    })
+})
